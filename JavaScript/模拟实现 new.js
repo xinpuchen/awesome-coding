926d112bf2c0202d8bcbeff19a20424cd5dc4a59
xinpu <xinpuchen@126.com>
2020-03-11 01:04:39 +0800

    feat: 增加 new 的模拟实现

---
+++ JavaScript/模拟实现 new.js
@@ -0,0 +1,19 @@
+/**
+ * new 运算符创建一个用户定义的对象类型的实例或具有构造函数的内置对象类型之一
+ *
+ * 要解决的问题：
+ * 1. 返回一个对象
+ * 2. 返回的对象可以访问到构造函数里的属性，也可以访问到构造函数原型上的属性
+ * 3. 构造函数是否有返回值，返回值是否有效（是否为一个对象）
+ */
+
+function mockNew() {
+  var obj = Object.create();
+  var constructor = Array.prototype.shift.call(arguments);
+  obj.__proto__ = constructor.prototype;
+  var result = constructor.apply(obj, arguments);
+  if (result && typeof result === "object") {
+    return result;
+  }
+  return obj;
+}
