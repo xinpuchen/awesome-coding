63e0af4ce04b9413f724b0bf04639935460b8a59
xinpu <xinpuchen@126.com>
2020-09-03 01:59:36 +0800

    feat: 反转链表

---
+++ LeetCode/linked-list/206.反转链表.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.14%)
+ * Likes:    1204
+ * Dislikes: 0
+ * Total Accepted:    321.6K
+ * Total Submissions: 456.5K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if(!head) return null;
+  let pre = null, cur = head;
+  while(cur){
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
+// @lc code=end
+
